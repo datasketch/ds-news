4687cc8ca488a6466656665362f36be22ab88bb1
David Daza <03dazal@gmail.com>
2019-06-05 10:25:09 -0500

    Set nunjucks as template engine

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 require('dotenv').config()
 
-const http = require('http')
-const express = require('express')
-const debug = require('debug')('ds-news')
 const connectToDB = require('./lib/db')
+const debug = require('debug')('ds-news')
+const express = require('express')
+const http = require('http')
+const nunjucks = require('nunjucks')
 
 const app = express()
 const port = process.env.PORT || 4000
+
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app
+})
 
 app.get('/', async (req, res, next) => {
   try {
     const db = await connectToDB()
     const posts = await db.collection('posts').find({}, { limit: 10 }).toArray()
-    res.json({ posts })
+    res.render('index.njk', { title: 'Datasketch News', posts })
   } catch (error) {
     debug(error)
     return next(error)
