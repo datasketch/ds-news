4750b74c84d9338315f2ac1b4a9fed9475fc2c6b
David Daza <03dazal@gmail.com>
2019-06-06 11:57:27 -0500

    Add methods for request all posts or a single post

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 require('dotenv').config()
 
-const connectToDB = require('./db')
 const debug = require('debug')('ds-news')
 const express = require('express')
 const http = require('http')
+const morgan = require('morgan')
 const path = require('path')
+const { getPosts, getPost } = require('./lib/post')
 
 const app = express()
 const port = process.env.PORT || 4000
@@ -12,21 +13,41 @@ const port = process.env.PORT || 4000
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
+app.use(morgan('dev'))
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', async (req, res, next) => {
   try {
-    const db = await connectToDB()
-    const posts = await db.collection('posts').find({}).sort({ publishedDate: -1 }).limit(20).toArray()
-    res.render('index', { title: 'Datasketch News', posts })
+    const posts = await getPosts()
+    res.render('index', { posts: posts.slice(1), featured: posts[0] })
   } catch (error) {
     debug(error)
     return next(error)
   }
 })
 
-app.use((req, res, next, error) => {
-  process.exit(1)
+app.get('/p/:slug', async (req, res, next) => {
+  try {
+    const post = await getPost(req.params.slug)
+    if (!post.length) {
+      return next()
+    }
+    res.json({ post: post[0] })
+  } catch (error) {
+    debug(error)
+    return next(error)
+  }
+})
+
+// Not found
+app.use((req, res, next) => {
+  res.render('404')
+})
+
+// Error handler
+app.use((error, req, res, next) => {
+  debug(error)
+  res.render('500')
 })
 
 const server = http.createServer(app)
