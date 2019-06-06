533c3268902b0a5f81055fbde3dff4ac37a063c8
David Daza <03dazal@gmail.com>
2019-06-06 13:54:33 -0500

    Initial configuration for i18n

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ require('dotenv').config()
 const debug = require('debug')('ds-news')
 const express = require('express')
 const http = require('http')
+const i18n = require('i18n')
+const menu = require('./menu')
+const moment = require('moment')
 const morgan = require('morgan')
 const path = require('path')
 const { getPosts, getPost } = require('./lib/post')
@@ -10,10 +13,19 @@ const { getPosts, getPost } = require('./lib/post')
 const app = express()
 const port = process.env.PORT || 4000
 
+i18n.configure({
+  locales: ['es', 'en'],
+  directory: path.join(__dirname, 'locales')
+})
+
+app.locals.menu = menu
+app.locals.moment = moment
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
 app.use(morgan('dev'))
+app.use(i18n.init)
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', async (req, res, next) => {
---
+++ menu.js
@@ -0,0 +1,32 @@
+module.exports = [
+  {
+    label: { es: 'Derechos Humanos', en: 'Human Rights' },
+    href: { es: 'derechos-humanos', en: 'human-rights' },
+    key: 'derechos-humanos'
+  },
+  {
+    label: { es: 'Género', en: 'Gender' },
+    href: { es: 'genero', en: 'gender' },
+    key: 'genero'
+  },
+  {
+    label: { es: 'Medio Ambiente', en: 'Environment' },
+    href: { es: 'medio-ambiente', en: 'environment' },
+    key: 'medio-ambiente'
+  },
+  {
+    label: { es: 'Arte y cultura', en: 'Art and Culture' },
+    href: { es: 'arte-y-cultura', en: 'art-and-culture' },
+    key: 'arte-y-cultura'
+  },
+  {
+    label: { es: 'Tecnología y sociedad', en: 'Technology and Society' },
+    href: { es: 'tecnologia-y-sociedad', en: 'technology-and-society' },
+    key: 'tecnologia-y-sociedad'
+  },
+  {
+    label: { es: 'Anticorrupción', en: 'Anticorruption' },
+    href: { es: 'anticorrupcion', en: 'anticorruption' },
+    key: 'anticorruption'
+  }
+]
