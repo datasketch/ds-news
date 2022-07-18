0c95dc63a291f61486dd3b49cb6bd75215e986dd
David Daza <03dazal@gmail.com>
2022-07-18 11:29:05 -0500

    Update default port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const debug = require('debug')('ds-news')
 const http = require('http')
 const app = require('./app')
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 3000
 const server = http.createServer(app)
 
 server.listen(port, () => debug(`Server up in http://localhost:${port}`))
